4f28d2dbe98e220178c0bd1d100082a3df2d0735
mitja <cmitja@gmail.com>
2020-01-04 18:42:12 +0100

    Implemented an option to restart the game.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { animationFrame } from 'rxjs/scheduler/animationFrame';
 import { interval } from 'rxjs/observable/interval';
 import { fromEvent } from 'rxjs/observable/fromEvent';
 import { combineLatest } from 'rxjs/observable/combineLatest';
+import { of } from 'rxjs/observable/of';
 
 import {
   map,
@@ -18,7 +19,9 @@ import {
   tap,
   skip,
   takeWhile,
-  take
+  take,
+  switchMap,
+  first
 } from 'rxjs/operators';
 
 import { DIRECTIONS, SPEED, SNAKE_LENGTH, FPS, APPLE_COUNT, POINTS_PER_APPLE } from './constants';
@@ -44,12 +47,13 @@ import {
   generateSnake,
   generateApples
 } from './utils';
+import { PartialObserver, Observer } from 'rxjs/Observer';
 
 /**
  * Create canvas element and append it to the page
  */
 let canvas = createCanvasElement();
-let ctx = canvas.getContext('2d');
+let ctx: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D;
 document.body.appendChild(canvas);
 
 /**
@@ -62,60 +66,97 @@ let click$ = fromEvent(document, 'click');
 let keydown$ = fromEvent(document, 'keydown');
 
 console.log('*********************');
-let direction$ = keydown$.pipe(
-  // tap((keyEvent: KeyboardEvent) => console.log('key: ' + keyEvent.keyCode)),
-  map((keyEvent: KeyboardEvent) => DIRECTIONS[keyEvent.keyCode]),
-  filter((value: Point2D) => !isNullOrUndefined(value)),
-  // tap((value: Point2D) => console.log('x: ' + value.x + ', y: ' + value.y)),
-  scan(nextDirection, INITIAL_DIRECTION),
-  startWith(INITIAL_DIRECTION),
-  distinctUntilChanged()
-);
 
-direction$
-  .subscribe((value: Point2D) => { console.log('Value emitted: ' + JSON.stringify(value)); }, (error) => {}, () => {});
+let createGame = (fps$: Observable<number>): Observable<Scene> => {
+  let direction$ = keydown$.pipe(
+    // tap((keyEvent: KeyboardEvent) => console.log('key: ' + keyEvent.keyCode)),
+    map((keyEvent: KeyboardEvent) => DIRECTIONS[keyEvent.keyCode]),
+    filter((value: Point2D) => !isNullOrUndefined(value)),
+    // tap((value: Point2D) => console.log('x: ' + value.x + ', y: ' + value.y)),
+    scan(nextDirection, INITIAL_DIRECTION),
+    startWith(INITIAL_DIRECTION),
+    distinctUntilChanged()
+  );
+
+  direction$
+    .subscribe((value: Point2D) => { console.log('Value emitted: ' + JSON.stringify(value)); }, (error) => {}, () => {});
 
 
-let length$ = new BehaviorSubject<number>(SNAKE_LENGTH);
-// let length$ = new ReplaySubject<number>(2);
+  let length$ = new BehaviorSubject<number>(SNAKE_LENGTH);
+  // let length$ = new ReplaySubject<number>(2);
 
-let snakeLength$ = length$.pipe(
-  scan((step: number, snakeLen: number) => step + snakeLen),
-  tap((value: number) => console.log('Snake length: ' + value)),
-  share()
-);
+  let snakeLength$ = length$.pipe(
+    scan((step: number, snakeLen: number) => step + snakeLen),
+    tap((value: number) => console.log('Snake length: ' + value)),
+    share()
+  );
 
-let score$ = snakeLength$.pipe(
-  startWith(0),
-  scan((score, _) => score + POINTS_PER_APPLE)
-);
+  let score$ = snakeLength$.pipe(
+    startWith(0),
+    scan((score, _) => score + POINTS_PER_APPLE)
+  );
 
-let snake$ = ticks$.pipe(
-  withLatestFrom(direction$, snakeLength$, (_, direction, snakeLength) => [direction, snakeLength]),
-  scan(move, generateSnake()),
-  share()
-);
+  let snake$ = ticks$.pipe(
+    withLatestFrom(direction$, snakeLength$, (_, direction, snakeLength) => [direction, snakeLength]),
+    scan(move, generateSnake()),
+    tap(() => 'snakeMoves...'),
+    share()
+  );
 
-let apples$ = snake$.pipe(
-  scan(eat, generateApples()),
-  distinctUntilChanged(),
-  share()
-);
+  let apples$ = snake$.pipe(
+    scan(eat, generateApples()),
+    distinctUntilChanged(),
+    share()
+  );
 
-let applesEaten$ = apples$.pipe(
-  skip(1),
-  tap(() => length$.next(POINTS_PER_APPLE)),
-).subscribe();
+  let applesEaten$ = apples$.pipe(
+    skip(1),
+    tap(() => length$.next(POINTS_PER_APPLE)),
+  ).subscribe();
 
-// snake$.subscribe(() => renderScene(ctx, null));
-let scene$ = combineLatest(snake$, apples$, score$, (snake, apples, score) => ({snake, apples, score}));
+  // snake$.subscribe(() => renderScene(ctx, null));
+  let scene$: Observable<Scene> = combineLatest(snake$, apples$, score$, (snake, apples, score) => ({snake, apples, score}));
 
-let game$ = interval(1000 / FPS, animationFrame).pipe(
-  withLatestFrom(scene$, (_, scene) => scene),
+  return fps$.pipe(
+    withLatestFrom(scene$, (_, scene: Scene) => scene)
+  );
+};
+
+// let game$ = interval(1000 / FPS, animationFrame).pipe(
+//   withLatestFrom(scene$, (_, scene) => scene),
+//   takeWhile((scene: Scene) => !isGameOver(scene))
+// ).subscribe((scene: Scene) => { renderScene(ctx, scene); }, () => {}, () => renderGameOver(ctx));
+
+let game$ =  of('Start the game').pipe(
+  tap(console.log),
+  map(() => interval(1000 / FPS, animationFrame)),
+  switchMap((fps$) => createGame(fps$)),
   takeWhile((scene: Scene) => !isGameOver(scene))
-).subscribe((scene: Scene) => { renderScene(ctx, scene); }, () => {}, () => renderGameOver(ctx));
+);
 
+let startGame = () => {
+  game$.subscribe(
+    (scene: Scene) => { renderScene(ctx, scene); },
+    (e: Error) => { console.log(e); },
+    () => {
+      renderGameOver(ctx);
 
+      click$.pipe(first()).subscribe(() => startGame());
+  });
+};
+
+startGame();
+
+export class GameResetter<Scene> implements Observer<Scene> {
+  constructor() {}
+  next (scene) { renderScene(ctx, scene); }
+  error(e: Error) { console.log(e); }
+  complete() {
+    renderGameOver(ctx);
+
+    click$.pipe(first()).subscribe(() => startGame());
+  }
+}
 
 
 
